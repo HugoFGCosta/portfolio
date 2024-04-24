2ca9c25a7d010a703e93adc638de4f53428702f6
HugoFGCosta <hugo.costa.T0126211@edu.atec.pt>
2024-04-24 14:31:46 +0100

    update foto

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburguer-icon");
+  const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
+}
+
+function closeMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.remove("open");
+  icon.classList.remove("open");
 }
 
 function toggleTheme() {
